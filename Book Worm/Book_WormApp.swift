340639861d65f3e6e4dbab8564e99edd87dc768a
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2023-02-22 13:16:00 -0500

    Initial Commit

---
+++ Book Worm/Book_WormApp.swift
@@ -0,0 +1,17 @@
+//
+//  Book_WormApp.swift
+//  Book Worm
+//
+//  Created by Parth Antala on 2023-02-22.
+//
+
+import SwiftUI
+
+@main
+struct Book_WormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Book Worm/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Book Worm
+//
+//  Created by Parth Antala on 2023-02-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
